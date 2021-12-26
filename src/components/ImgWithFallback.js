4ce29dfff231fd618465ac830d6aaa3163a3d0c7
MatchuPitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-12-26 11:09:13 +0100

    Update ImgWithFallback.js

--- src/components/ImgWithFallback.js
+++ src/components/ImgWithFallback.js
@@ -2,7 +2,7 @@ const ImgWithFallback = ({ className, src, fallback, alt, type = 'image/webp', .
   return (
     <picture className={className} >
       <source srcSet={src} type={type} />
-      <img src={fallback} alt={alt} {...rest} className={className} />
+      <img src={fallback} alt={alt} {...rest} />
     </picture>
   );
 };
