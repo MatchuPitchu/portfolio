dac49c142d55b8efc9cd6dfd902e6c0c3446852b
MatchuPitchu <michael.flohr@posteo.de>
2022-01-10 11:16:35 +0100

    update styling networks and add researchgate

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -5,6 +5,7 @@ import classes from './Footer.module.css';
 
 import linkedin from '../../assets/logos/logo-linkedin.svg';
 import xing from '../../assets/logos/logo-xing.svg';
+import researchgate from '../../assets/logos/logo-ResearchGate.svg';
 import github from '../../assets/logos/logo-github.svg';
 import githubDark from '../../assets/logos/logo-github-dark.svg';
 
@@ -23,6 +24,13 @@ const Footer = () => {
         </a>
         <a href='https://www.xing.com/profile/Michael_Flohr9' rel='noreferrer' target='_blank'>
           <img className={classes.logo} src={xing} alt='Logo Xing' />
+        </a>
+        <a
+          href='https://www.researchgate.net/profile/Michael-Flohr'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <img className={classes.logo} src={researchgate} alt='Logo ResearchGate' />
         </a>
         <a href='https://github.com/MatchuPitchu' rel='noreferrer' target='_blank'>
           <img className={classes.logo} src={isLight ? github : githubDark} alt='Logo GitHub' />
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,19 +1,17 @@
-import React from 'react';
-
 import classes from './Button.module.css';
 
 const width = '105';
 const height = '50';
 
-const Button = (props) => {
+const Button = ({ type, className, onClick, disabled, ariaLabel, children }) => {
   return (
     <div className={classes['btn-container']}>
       <button
-        type={props.type || 'button'}
-        className={`${classes.btn} ${props.className}`}
-        onClick={props.onClick}
-        disabled={props.disabled}
-        aria-label={props.ariaLabel}
+        type={type || 'button'}
+        className={`${classes.btn} ${className}`}
+        onClick={onClick}
+        disabled={disabled}
+        aria-label={ariaLabel}
       >
         <svg width={`${width}px`} height={`${height}px`} viewBox={`0 0 ${width} ${height}`}>
           <polyline
@@ -23,7 +21,7 @@ const Button = (props) => {
             points={`${width - 1},1 ${width - 1},${height - 1} 1,${height - 1} 1,1 ${width - 1},1`}
           ></polyline>
         </svg>
-        {props.children}
+        {children}
       </button>
     </div>
   );
