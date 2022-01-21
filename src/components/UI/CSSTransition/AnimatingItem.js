dbaca0bb9abaceb5168b0a305d6ea22109b8eec4
MatchuPitchu <michael.flohr@posteo.de>
2022-01-21 22:54:19 +0100

    improve csstransition

--- src/components/UI/CSSTransition/AnimatingItem.js
+++ src/components/UI/CSSTransition/AnimatingItem.js
@@ -1,13 +1,18 @@
 import CSSTransition from 'react-transition-group/CSSTransition';
 import classes from './Animating.module.css';
 
+const animationTiming = {
+  enter: 500,
+  exit: 0,
+};
+
 const AnimatingItem = ({ show, children }) => {
   return (
     <CSSTransition
       mountOnEnter
       unmountOnExit
       in={show}
-      timeout={500}
+      timeout={animationTiming}
       classNames={{
         enter: classes.enter,
         enterActive: classes.enterActive,
