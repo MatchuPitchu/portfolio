1dd0574e34483993fbd42a9565fe7d0e68212084
MatchuPitchu <michael.flohr@posteo.de>
2022-01-29 22:11:39 +0100

    text

--- src/components/About/Hero.js
+++ src/components/About/Hero.js
@@ -18,10 +18,10 @@ const Hero = () => {
           <p>
             Studium, Engagement und berufliche Aktivitäten führten mich in allerlei Themenfelder:
             von Musik, über Wirtschaft, Politik, Kultur, interkulturelle Kommunikation, nachhaltige
-            Entwicklung, Digitalisierung bis zur Full-Stack Web- & App-Entwicklung. Jedes Puzzleteil
-            hat meinen Blick auf die Welt verändert und mich bereichert. Sowieso bin ich davon
-            überzeugt: Nur inter- und transdiziplinär lässt sich die Welt verstehen und zu einem
-            nachhaltigeren Ort verwandeln.
+            Entwicklung, Digitalisierung bis zur Web- & App-Entwicklung. Jedes Puzzleteil hat meinen
+            Blick auf die Welt verändert und mich bereichert. Sowieso bin ich davon überzeugt: Nur
+            inter- und transdiziplinär lässt sich die Welt verstehen und zu einem nachhaltigeren Ort
+            verwandeln.
           </p>
         </div>
         <div className={classes.images}>
