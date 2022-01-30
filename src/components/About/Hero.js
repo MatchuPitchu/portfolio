a7e2e9785babf17c39ca7e58364f15f193335f54
MatchuPitchu <michael.flohr@posteo.de>
2022-01-30 21:54:39 +0100

    adjuste text landingpage

--- src/components/About/Hero.js
+++ src/components/About/Hero.js
@@ -7,21 +7,23 @@ import classes from './Hero.module.css';
 const Hero = () => {
   return (
     <section className={classes.hero}>
-      <H1Title>
-        <div className={classes.title}>Dr</div>
-        <div className={classes.firstName}>Michael</div>
-        <div className={classes.lastName}>Flohr</div>
+      <H1Title classTitle={classes.h1}>
+        <span className={classes.title}>Dr</span>
+        <span className={classes.firstName}>Michael</span>
+        <span className={classes.lastName}>Flohr</span>
       </H1Title>
       <Container className={classes['container-customized']}>
         <div className={classes.presentation}>
           <h2>über mich</h2>
           <p>
             Studium, Engagement und berufliche Aktivitäten führten mich in allerlei Themenfelder:
-            von Musik, über Wirtschaft, Politik, Kultur, interkulturelle Kommunikation, nachhaltige
-            Entwicklung, Digitalisierung bis zur Web- & App-Entwicklung. Jedes Puzzleteil hat meinen
-            Blick auf die Welt verändert und mich bereichert. Sowieso bin ich davon überzeugt: Nur
-            inter- und transdiziplinär lässt sich die Welt verstehen und zu einem nachhaltigeren Ort
-            verwandeln.
+            von Musik, über Wirtschaft, Politik, Kulturmanagement, interkulturelle Kommunikation,
+            nachhaltige Entwicklung, Digitalisierung bis zur Web- & App-Entwicklung. Jedes
+            Puzzleteil hat mir eine neue Perspektive eröffnet und mich bereichert. Sowieso bin ich
+            davon überzeugt: Nur mit dem Blick über den eigenen Tellerrand hinaus und dem Suchen
+            jenseits des eigenen Horizonts – also inter- und transdiziplinär – lässt sich die Welt
+            erfassen; und so gestalten, dass sie ein nachhaltigerer, lebenswerter und
+            zukunftsfähiger Ort für uns Menschen wird.
           </p>
         </div>
         <div className={classes.images}>
